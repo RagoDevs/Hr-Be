6b9be4538b2e0a4528fb76203d74d438b7d4a62b
Hopertz <lugano.ngulwa@gmail.com>
2024-04-29 04:27:15 +0300

    add routes

---
+++ cmd/api/routes.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/swaggo/echo-swagger"
+
+	_ "github.com/swaggo/echo-swagger/example/docs"
+)
+
+func (app *application) routes() *echo.Echo {
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	DefaultCORSConfig := middleware.CORSConfig{
+		Skipper:      middleware.DefaultSkipper,
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	}
+
+	e.Use(middleware.CORSWithConfig(DefaultCORSConfig))
+
+	e.GET("/ping", app.pingHandler)
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+
+
+
+	return e
+
+}
