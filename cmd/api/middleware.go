fffde4be100951d6daab4cef8619990659ad812e
Hopertz <lugano.ngulwa@gmail.com>
2024-04-30 11:55:02 +0300

    add authenicate & require enabled user middleware

---
+++ cmd/api/middleware.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"crypto/sha256"
+	"database/sql"
+	"errors"
+	"log/slog"
+	"net/http"
+	"strings"
+	"time"
+
+	db "github.com/Hopertz/Hr-Be/internal/db/sqlc"
+	"github.com/Hopertz/Hr-Be/internal/token"
+	"github.com/labstack/echo/v4"
+)
+
+func (app *application) authenticate(next echo.HandlerFunc) echo.HandlerFunc {
+
+	return func(c echo.Context) error {
+
+		authorizationHeader := c.Request().Header.Get("Authorization")
+
+		if authorizationHeader == "" {
+			return c.JSON(http.StatusUnauthorized, map[string]string{"error": "unauthorized"})
+		}
+
+		headerParts := strings.Split(authorizationHeader, " ")
+
+		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+
+			return c.JSON(http.StatusUnauthorized, map[string]string{"error": "invalid auth token"})
+
+		}
+
+		tokenString := headerParts[1]
+		tokenHash := sha256.Sum256([]byte(tokenString))
+
+		params := db.GetUserByTokenParams{
+			Hash:   tokenHash[:],
+			Scope:  token.ScopeAuthentication,
+			Expiry: time.Now(),
+		}
+
+		user, err := app.store.GetUserByToken(c.Request().Context(), params)
+
+		if err != nil {
+			switch {
+			case errors.Is(err, sql.ErrNoRows):
+				return c.JSON(http.StatusUnauthorized, map[string]string{"error": "invalid or expired auth token"})
+			default:
+				slog.Error("Error on getting user associated with token ", "Error", err.Error())
+				return c.JSON(http.StatusInternalServerError, map[string]string{"error": "internal server error"})
+			}
+		}
+
+		c.Set("user", user)
+
+		return next(c)
+
+	}
+
+}
+
+
+func (app *application) requireEnabledUser(next echo.HandlerFunc) echo.HandlerFunc {
+
+	return func(c echo.Context) error {
+		user, ok := c.Get("user").(*db.GetUserByTokenRow)
+
+		if !ok || user == nil || !user.IsEnabled {
+			return c.JSON(http.StatusUnauthorized, "you are disabled user")
+		}
+		
+		return next(c)
+	}
+}
+
