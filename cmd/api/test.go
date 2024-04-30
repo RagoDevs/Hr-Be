7b8be410024737b6e1583d88a043edf63d5f0c8e
Hopertz <lugano.ngulwa@gmail.com>
2024-04-30 13:04:58 +0300

    add test handler (testing auth)

---
+++ cmd/api/test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func (app *application) testHandler(c echo.Context) error {
+
+	test := map[string]string {
+		"test": "test test test",
+	}
+	return c.JSON(http.StatusOK, test)
+
+}
