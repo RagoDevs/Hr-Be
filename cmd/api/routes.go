fd806d30237588e61119cf85cca8f806b5b7a36d
Hopertz <lugano.ngulwa@gmail.com>
2024-04-30 13:04:34 +0300

    add rquired enabled user as global middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/swaggo/echo-swagger"
+	echoSwagger "github.com/swaggo/echo-swagger"
 
 	_ "github.com/swaggo/echo-swagger/example/docs"
 )
@@ -28,7 +28,12 @@ func (app *application) routes() *echo.Echo {
 	e.POST("/login", app.login)
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
+	g := e.Group("/auth")
 
+	g.Use(app.authenticate)
+	g.Use(app.requireEnabledUser)
+
+	g.GET("/test", app.testHandler)
 
 	return e
 
