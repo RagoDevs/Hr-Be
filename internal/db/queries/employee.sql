1086a2bde7e4f828ca491e4f1f010dae8d54b649
Hopertz <lugano.ngulwa@gmail.com>
2024-05-01 18:22:38 +0300

    add employees models

---
+++ internal/db/queries/employee.sql
@@ -0,0 +1,113 @@
+-- name: CreateEmployee :exec
+INSERT INTO employee (
+    user_id, 
+    name, 
+    dob, 
+    avatar, 
+    phone, 
+    gender, 
+    job_title, 
+    department, 
+    address, 
+    joining_date
+)
+VALUES (
+    $1, 
+    $2, 
+    $3, 
+    $4, 
+    $5, 
+    $6, 
+    $7, 
+    $8, 
+    $9, 
+    $10
+);
+
+-- name: GetEmployeeById :one
+SELECT * FROM  employee  WHERE employee.id = $1;
+
+
+-- name: UpdateEmployeeById :exec
+UPDATE employee
+SET
+    name = $1,
+    dob = $2,
+    avatar = $3,
+    phone = $4,
+    gender = $5,
+    job_title = $6,
+    department = $7,
+    address = $8,
+    joining_date = $9
+
+WHERE
+    id = $10;
+
+
+
+-- name: DeleteEmployeeById :exec
+DELETE FROM employee
+WHERE id = $1;
+
+
+
+-- name: GetAllEmployees :many
+SELECT
+    e.id AS employee_id,
+    e.user_id ,
+    u.email ,
+    u.is_enabled ,
+    u.role_id ,
+    r.name AS role_name,
+    e.name AS employee_name,
+    e.dob ,
+    e.avatar ,
+    e.phone ,
+    e.gender ,
+    e.job_title ,
+    e.department ,
+    e.address ,
+    e.joining_date ,
+    e.created_at 
+FROM
+    employee e
+JOIN
+    users u ON e.user_id = u.id
+
+JOIN 
+    role r ON r.id =  u.role_id
+
+ORDER BY
+    e.created_at DESC;
+
+
+
+-- name: GetEmployeeByIdDetailed :one
+SELECT
+    e.id AS employee_id,
+    e.user_id ,
+    u.email ,
+    u.is_enabled ,
+    u.role_id ,
+    r.name AS role_name,
+    e.name AS employee_name,
+    e.dob ,
+    e.avatar ,
+    e.phone ,
+    e.gender ,
+    e.job_title ,
+    e.department ,
+    e.address ,
+    e.joining_date ,
+    e.created_at 
+FROM
+    employee e
+JOIN
+    users u ON e.user_id = u.id
+
+JOIN 
+    role r ON r.id =  u.role_id
+
+WHERE 
+    e.id = $1;
--- internal/db/sqlc/querier.go
+++ internal/db/sqlc/querier.go
@@ -12,20 +12,26 @@ import (
 
 type Querier interface {
 	CreateContract(ctx context.Context, arg CreateContractParams) error
+	CreateEmployee(ctx context.Context, arg CreateEmployeeParams) error
 	CreateLeave(ctx context.Context, arg CreateLeaveParams) error
 	CreateToken(ctx context.Context, arg CreateTokenParams) error
 	CreateUser(ctx context.Context, arg CreateUserParams) (User, error)
 	DeleteContract(ctx context.Context, id uuid.UUID) error
+	DeleteEmployeeById(ctx context.Context, id uuid.UUID) error
 	DeleteLeave(ctx context.Context, id uuid.UUID) error
 	GetAllContracts(ctx context.Context) ([]GetAllContractsRow, error)
+	GetAllEmployees(ctx context.Context) ([]GetAllEmployeesRow, error)
 	GetAllLeaves(ctx context.Context) ([]GetAllLeavesRow, error)
 	GetContractById(ctx context.Context, id uuid.UUID) (Contract, error)
 	GetContractByIdDetailed(ctx context.Context, id uuid.UUID) (GetContractByIdDetailedRow, error)
+	GetEmployeeById(ctx context.Context, id uuid.UUID) (Employee, error)
+	GetEmployeeByIdDetailed(ctx context.Context, id uuid.UUID) (GetEmployeeByIdDetailedRow, error)
 	GetLeaveById(ctx context.Context, id uuid.UUID) (Leave, error)
 	GetLeaveByIdDetailed(ctx context.Context, id uuid.UUID) (GetLeaveByIdDetailedRow, error)
 	GetUserByEmail(ctx context.Context, email string) (GetUserByEmailRow, error)
 	GetUserByToken(ctx context.Context, arg GetUserByTokenParams) (GetUserByTokenRow, error)
 	UpdateContract(ctx context.Context, arg UpdateContractParams) error
+	UpdateEmployeeById(ctx context.Context, arg UpdateEmployeeByIdParams) error
 	UpdateLeave(ctx context.Context, arg UpdateLeaveParams) error
 }
 
