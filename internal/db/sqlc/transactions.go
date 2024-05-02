1e3856e1536a9a29c6dab4c1934525a619e97121
Hopertz <lugano.ngulwa@gmail.com>
2024-05-02 15:08:36 +0300

    add transactions for user & employee

---
+++ internal/db/sqlc/transactions.go
@@ -0,0 +1,84 @@
+package db
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type TxnUserEmployeeDelete struct {
+	UserId     uuid.UUID
+	EmployeeId uuid.UUID
+}
+
+func (store *SQLStore) TxnCreateUserEmployee(ctx context.Context, user CreateUserParams, emp CreateEmployeeParams) error {
+
+	tx, err := store.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	qtx := New(tx)
+
+	usr, err := qtx.CreateUser(ctx, user)
+
+	if err != nil {
+		return err
+	}
+
+	emp.UserID = usr.ID
+
+	if err = qtx.CreateEmployee(ctx, emp); err != nil {
+		return err
+	}
+
+	return tx.Commit()
+
+}
+
+func (store *SQLStore) TxnUpdateUserEmployee(ctx context.Context, user UpdateUserByIdParams, emp UpdateEmployeeByIdParams) error {
+
+	tx, err := store.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	qtx := New(tx)
+
+	err = qtx.UpdateUserById(ctx, user)
+
+	if err != nil {
+		return err
+	}
+	if err = qtx.UpdateEmployeeById(ctx, emp); err != nil {
+		return err
+	}
+
+	return tx.Commit()
+
+}
+
+func (store *SQLStore) TxnDeleteUserEmployee(ctx context.Context, args TxnUserEmployeeDelete) error {
+
+	tx, err := store.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	qtx := New(tx)
+
+	err = qtx.DeleteEmployeeById(ctx, args.EmployeeId)
+
+	if err != nil {
+		return err
+	}
+	if err = qtx.DeleteUserById(ctx, args.UserId); err != nil {
+		return err
+	}
+
+	return tx.Commit()
+
+}
