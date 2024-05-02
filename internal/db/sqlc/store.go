db175c8cf16453da8a4c0ac165ac56d0a3bb65bc
Hopertz <lugano.ngulwa@gmail.com>
2024-05-02 15:11:19 +0300

    add transactions on Store interface

--- internal/db/sqlc/store.go
+++ internal/db/sqlc/store.go
@@ -1,11 +1,15 @@
 package db
 
 import (
+	"context"
 	"database/sql"
 )
 
 type Store interface {
 	Querier
+	TxnCreateUserEmployee(ctx context.Context, user CreateUserParams, emp CreateEmployeeParams) error
+    TxnUpdateUserEmployee(ctx context.Context, user UpdateUserByIdParams, emp UpdateEmployeeByIdParams) error
+	TxnDeleteUserEmployee(ctx context.Context, args TxnUserEmployeeDelete) error 
 }
 
 type SQLStore struct {
